7bf3511b2cf2543d514377534d8166f2b03da943
jihyeon Hwang <jihyeon1804@gmail.com>
2021-07-30 16:46:55 +0900

    홈 - 메인 - 로그인 화면까지 바로 연결되는 1차 세팅

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
+from django.views.generic.base import TemplateView
 
 app_name = 'music'
 
 urlpatterns = [
-    path('', )
+    path('', TemplateView.as_view(template_name='music/music_home.html'), name='music_home'),
+    path('login/', views.login, name='login')
 ]
--- music/views.py
+++ music/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import LoginForm
+
 
 # Create your views here.
+def login(request):
+    form = LoginForm()
+    return render(request, 'music/login.html', {'form': form})  # 응답 데이터 전달
