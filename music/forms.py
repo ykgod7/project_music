1e83eacd476f09db8ef11ba043457d85a85d4ba8
jihyeon Hwang <jihyeon1804@gmail.com>
2021-07-30 17:38:44 +0900

    홈 - 메인 - 로그인 회원 코드 넣기 전(DB 모델이 필요한 것 같아서 구분)

--- music/forms.py
+++ music/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.contrib.auth.hashers import check_password  # 패스워드 비교용
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=32)
-    password = forms.CharField()
+    username = forms.CharField(label="아이디", max_length=32,
+                               error_messages={'required': "아이디를 입력하세요"})
+    password = forms.CharField(label="비밀번호", widget=forms.PasswordInput,
+                               error_messages={'required': "비밀번호를 입력하세요"})
+
--- music/views.py
+++ music/views.py
@@ -4,5 +4,11 @@ from .forms import LoginForm
 
 # Create your views here.
 def login(request):
-    form = LoginForm()
+    if request.method=="POST":
+        form=LoginForm(request.POST)
+        # 정상적인 데이터인지 확인
+        if form.is_valid():
+            return redirect('/')
+    else:
+        form = LoginForm()
     return render(request, 'music/login.html', {'form': form})  # 응답 데이터 전달
