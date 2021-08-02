b7336895d02609e700d51eae7451e6c5626182ba
ykgod7 <ejihwan7@gmail.com>
2021-08-02 19:10:26 +1400

    마이페이지 추가

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'music'
 
 urlpatterns = [
-    path('', )
+    path('<str:username>', views.mypage, name='mypage')
 ]
--- music/views.py
+++ music/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+def mypage(request, username):
+    user = get_user_model()
+    return render(request, 'mypage.html', {'user': user})
+
+
