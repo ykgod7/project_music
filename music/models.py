c4981947f79bbdb83ea25679dd62623f0edd8521
Moon Youl <dsw9999@naver.com>
2021-08-03 13:14:21 +0900

    음악 세부사항 화면 + popup 창

--- music/models.py
+++ music/models.py
@@ -1,27 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Artist(models.Model):     # 프레젠테이션..?
+class Artist(models.Model):
     a_name = models.CharField(max_length=50)
 
 
-class Music(models.Model):      # 프레젠테이션 7p
+class Music(models.Model):
     m_like = models.IntegerField(default=0)
     m_title = models.CharField(max_length=100)
-    artist_fk = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    # artist_fk = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
 
-class MyPlaylist(models.Model):     # 프레젠테이션 8p
+class MyPlaylist(models.Model):
+    mp_title = models.CharField(max_length=100)
+    mp_like = models.IntegerField(default=0)
+
+
+class Playlist(models.Model):
     music_fk = models.ForeignKey(Music, on_delete=models.CASCADE)
-
-
-class Playlist(models.Model):       # 프레젠테이션 5p
-    p_like = models.IntegerField(default=0)
     myplaylist_fk = models.ForeignKey(MyPlaylist, on_delete=models.CASCADE)
+    user_fk = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
-class PlaylistComment(models.Model):        # 프레젠테이션 5p
+class PlaylistComment(models.Model):
     c_contents = models.TextField()
+    user_fk = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     myplaylist_fk = models.ForeignKey(MyPlaylist, on_delete=models.CASCADE)
 
 
