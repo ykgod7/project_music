979a90c3323d4739bce246b044b1a6c7f51b812c
Moon Youl <dsw9999@naver.com>
2021-08-03 14:49:31 +0900

    음악 세부사항 화면 + popup 창(ver5)

--- music/forms.py
+++ music/forms.py
@@ -1,12 +1,8 @@
 from django import forms
-from django.contrib.auth.hashers import check_password  # 패스워드 비교용
+
 from .models import MyPlaylist
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label="아이디", max_length=32,
-                               error_messages={'required': "아이디를 입력하세요"})
-    password = forms.CharField(label="비밀번호", widget=forms.PasswordInput,
-                               error_messages={'required': "비밀번호를 입력하세요"})
+
 
 class MyplayListForm(forms.ModelForm):
     class Meta:
@@ -20,4 +16,5 @@ class MyplayListForm(forms.ModelForm):
                 }
             ),
         }
+
 
