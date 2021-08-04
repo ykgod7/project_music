96a924b4f6b84d5756f50d32e5a9644561bc54b2
ykgod7 <ejihwan7@gmail.com>
2021-08-04 19:22:43 +1400

    경진씨 css merging 완료

--- music/views.py
+++ music/views.py
@@ -20,17 +20,12 @@ def m_music_rank_title(request):
     return render(request, 'music_rank.html', {'info_music': info_music, 'select': 'f_title'})
 
 
-# index.html : select
 def m_list_like(request):
-
-    # info_list = models.MyPlaylist.objects.all().order_by('p_like')
-    playlists = models.MyPlaylist.objects.all().order_by('p_like')
+    playlists = models.MyPlaylist.objects.all().order_by('mp_like')
     return render(request, 'index.html', {'playlists': playlists, 'select': 'l_like'})
 
 
 def m_list_new(request):
-
-    # info_list = models.MyPlaylist.objects.all().order_by('list_pub_date')
     playlists = models.MyPlaylist.objects.all().order_by('list_pub_date')
     return render(request, 'index.html', {'playlists': playlists, 'select': 'l_new'})
 
