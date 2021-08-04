fbe05e632fbd57d1c96fcfb0e09b5f1c86223835
jihyeon Hwang <jihyeon1804@gmail.com>
2021-08-04 10:15:13 +0900

    메인 화면 select 기능 추가

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,7 @@ app_name = 'music'
 urlpatterns = [
     path('music_rank/f_like', views.m_music_rank_like, name='music_rank_like'),
     path('music_rank/f_title', views.m_music_rank_title, name='music_rank_title'),
+    path('l_like', views.m_list_like, name='list_like'),
+    path('l_new', views.m_list_new, name='list_new'),
     path('<str:username>', views.mypage, name='mypage')
 ]
--- static/js/js.js
+++
@@ -1,5 +0,0 @@
-$(function(event){
-    $('#music_rank_btn').on('click',function(event){
-        document.location.href = '/music/music_rank/'
-    })
-})
