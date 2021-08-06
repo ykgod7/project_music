a5cf227cb4664e1954c7ce2d0e62fb3495b3437b
jihyeon Hwang <jihyeon1804@gmail.com>
2021-08-06 14:44:23 +0900

    영상 재생까지

--- static/js/search.js
+++ static/js/search.js
@@ -10,7 +10,7 @@ $(function(event) {
                 //alert('검색 진입')
                 $("#list").empty();
                 let musicList = response["results"]["trackmatches"]["track"];
-                for (let i = 0; i < 5; i++) {	//반복문을 쓰는 이유는 track 내에 이름을 모두 출력하기 위함. '3' 위치에 나중에 넣을 변수 : musicList.length
+                for (let i = 0; i < 2; i++) {	//반복문을 쓰는 이유는 track 내에 이름을 모두 출력하기 위함. '3' 위치에 나중에 넣을 변수 : musicList.length
                     let albumTitle = musicList[i]["name"]
                     let albumArtist = musicList[i]["artist"]
                     let albumUrl = musicList[i]["url"]
@@ -18,8 +18,8 @@ $(function(event) {
                     /////// 썸네일 동영상 ID값을 가져오기 위한 ajax 추가 영역
 
                     //let apikey = "AIzaSyB1nDzwhecsIlKfq3LmBHxRoO6x6yVn7iQ";   //이문열
-                    let apikey = "AIzaSyCfuyiH7W-NJQTVdOjt8UC9SYoUZJegnOk";   //장경진
-                    //let apikey = "AIzaSyCXqe22SwyypvUQojB9UIjONWPqfwyWNs8"  //황지현
+                    //let apikey = "AIzaSyCfuyiH7W-NJQTVdOjt8UC9SYoUZJegnOk";   //장경진
+                    let apikey = "AIzaSyCXqe22SwyypvUQojB9UIjONWPqfwyWNs8"  //황지현
                     let q_keyword = albumTitle + ' ' + albumArtist + ' official'
                     console.log(q_keyword);
                     (function(i){
@@ -36,13 +36,22 @@ $(function(event) {
                                 html += '<div class="container" style="text-align:center; height:240px">' +
                                     '<div class="container" style="display:inline-block; text-align:center; border-radius: 15%; margin-top:20px; width:360px; height:220px; background-color: #BECDFF;">' +
                                     '<iframe style="margin-top:20px;" width="320" height="180" src="https://img.youtube.com/vi/'+varId+'/mqdefault.jpg"></iframe><br>' +
-                                    '<a href="music/musicvideo/">' +
+                                    '<a href="music/musicvideo/'+varId+'/">' +
                                     albumArtist + ' : ' + albumTitle +
                                     '</a></div></div>'
                                 $("#list").append(html);
                             },
                             error : function(err) {
-                                console.log(err)
+                                console.log(err+'\n샘플 videoId=BlackPink:Kill This Love')
+                                var html = '';
+                                var varId = '2S24-y0Ij3Y';
+                                html += '<div class="container" style="text-align:center; height:240px">' +
+                                    '<div class="container" style="display:inline-block; text-align:center; border-radius: 15%; margin-top:20px; width:360px; height:220px; background-color: #BECDFF;">' +
+                                    '<iframe style="margin-top:20px;" width="320" height="180" src="https://img.youtube.com/vi/2S24-y0Ij3Y/mqdefault.jpg"></iframe><br>' +
+                                    '<a href="music/musicvideo/'+varId+'/">' +
+                                    albumArtist + ' : ' + albumTitle +
+                                    '</a></div></div>'
+                                $("#list").append(html);
                             }
                         })
                     })(i);
