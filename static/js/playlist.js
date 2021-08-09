4d8b85bac0e2db3b23d1245a1c1de3215f94a56f
Moon Youl <dsw9999@naver.com>
2021-08-09 11:23:20 +0900

    Fix: Playlist.js 수정

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,9 +1,46 @@
 
 $(function (event) {
-    $("#login_disable").leanModal({
+    $("#modal_trigger").leanModal({
         top: 100,
         overlay: 0.5,
         closeButton: ".modal_close",
         addButton: ".modal_add",
     });
+    login()
 });
+
+function login() {
+    $('#login_submit').click(function () {
+        var csrf_token = $('[name=csrfmiddlewaretoken]').val();
+        console.log($('#username').val())
+        console.log(1)
+        console.log($('#password').val())
+        console.log(2)
+        console.log($('.popuplogin_form').serialize())
+        $.ajax({
+            url: '/user/popuplogin/',
+            type: 'post',
+            dataType: 'json',
+            headers: {
+                'X-CSRFToken': csrf_token
+            },
+            data: $('.popuplogin_form').serialize(),
+            success: function (response) {
+                if (response.result == 'true') {
+                    location.reload()
+                    console.log('로그인됨')
+                } else {
+                    console.log('로그인 안됨')
+                    console.log(response.con)
+                    console.log(22)
+                    $('err_msg').attr('display', '')
+                }
+
+            },
+            error: function (response) {
+                alert('뭔가 잘못됨')
+                alert(response.result)
+            }
+        })
+    })
+};
